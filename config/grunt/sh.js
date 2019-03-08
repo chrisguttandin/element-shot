878e8b9b4749bdb0093c19f0382cd0244e69421d
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-03-08 23:42:57 +0100

    build(config): update grunt config

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -6,6 +6,6 @@ module.exports = {
         cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
     },
     'test-integration': {
-        cmd: 'mocha --bail --require config/mocha/config-integration.js test/integration'
+        cmd: 'mocha --bail --recursive --require config/mocha/config-integration.js test/integration'
     }
 };
