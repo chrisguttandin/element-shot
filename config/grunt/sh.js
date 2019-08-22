2cc71ca1d4a11a83c60e7e3c8509168f5e22e559
Christoph Guttandin <chrisguttandin@media-codings.com>
2019-08-22 16:26:44 +0200

    build(config): update linting tasks

--- config/grunt/sh.js
+++ config/grunt/sh.js
@@ -1,14 +1,24 @@
-module.exports = {
-    'build': {
-        cmd: 'tsc -p src/tsconfig.json'
-    },
-    'lint': {
-        cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
-    },
-    'test-e2e': {
-        cmd: 'webdriver-manager update --gecko false && protractor config/protractor/config.js'
-    },
-    'test-integration': {
-        cmd: 'mocha --bail --recursive --require config/mocha/config-integration.js test/integration'
-    }
+module.exports = (grunt) => {
+    const fix = (grunt.option('fix') === true);
+
+    return {
+        'build': {
+            cmd: 'tsc -p src/tsconfig.json'
+        },
+        'lint-config': {
+            cmd: `eslint --config config/eslint/config.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives *.js config/**/*.js`
+        },
+        'lint-src': {
+            cmd: 'tslint --config config/tslint/src.json --project src/tsconfig.json src/*.ts src/**/*.ts'
+        },
+        'lint-test': {
+            cmd: `eslint --config config/eslint/test.json ${ (fix) ? '--fix ' : '' }--report-unused-disable-directives test/**/*.js`
+        },
+        'test-e2e': {
+            cmd: 'webdriver-manager update --gecko false && protractor config/protractor/config.js'
+        },
+        'test-integration': {
+            cmd: 'mocha --bail --recursive --require config/mocha/config-integration.js test/integration'
+        }
+    };
 };
