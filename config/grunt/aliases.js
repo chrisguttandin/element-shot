7134b075e34761d70134e92f2e6660ea0f0051cc
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-01-02 23:13:46 +0100

    build(config): run babel with the default CLI

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -5,7 +5,7 @@ const filter = (predicate, ...tasks) => (predicate ? tasks : []);
 const isType = (...types) => env.TYPE === undefined || types.includes(env.TYPE);
 
 module.exports = {
-    build: ['clean:build', 'sh:build', 'babel:build'],
+    build: ['clean:build', 'sh:build'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
     test: ['build', ...filter(isType('e2e'), 'sh:test-e2e'), ...filter(isType('integration'), 'sh:test-integration')]
 };
