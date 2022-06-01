88f152066b3fa059a2a765b180ee77ca94a02aff
Christoph Guttandin <chrisguttandin@media-codings.com>
2022-06-01 05:55:40 +0200

    build(config): use rimraf to delete build directory

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -5,7 +5,7 @@ const filter = (predicate, ...tasks) => (predicate ? tasks : []);
 const isType = (...types) => env.TYPE === undefined || types.includes(env.TYPE);
 
 module.exports = {
-    build: ['clean:build', 'sh:build'],
+    build: ['sh:clean', 'sh:build'],
     lint: ['sh:lint-config', 'sh:lint-src', 'sh:lint-test'],
     test: ['build', ...filter(isType('e2e'), 'sh:test-e2e'), ...filter(isType('integration'), 'sh:test-integration')]
 };
